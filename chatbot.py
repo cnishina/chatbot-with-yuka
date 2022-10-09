f51e6499d63d4c53496fe3deacd9da6b7939478c
Craig Nishina <cnishina@users.noreply.github.com>
2022-10-09 12:52:34 -0700

    feat: add basic chat focus command (#1)

---
+++ chatbot.py
@@ -0,0 +1,30 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+from twitchio.ext import commands
+
+
+load_dotenv()
+_ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+
+
+class Bot(commands.Bot):
+    def __init__(self):
+        # Initialise our Bot with our access token, prefix and a list of channels
+        # to join on boot...
+        super().__init__(
+            token=_ACCESS_TOKEN,
+            prefix="!",
+            initial_channels=["bedtimebear_808"],
+        )
+
+    async def event_ready(self):
+        # We are logged in and ready to chat and use commands...
+        print(f"Logged in as | {self.nick}")
+        print(f"User id is | {self.user_id}")
+
+    @commands.command()
+    async def focus(self, ctx: commands.Context):
+        author = ctx.message.author.name
+        message = ctx.message.content.replace("!focus", "")
+        await ctx.send(f"{author} is focusing: {message}.")
